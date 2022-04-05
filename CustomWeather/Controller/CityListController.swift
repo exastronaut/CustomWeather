4470d44971f8752f06ece60ba8893d6a911b77e2
Artem Sviridov <exastronaut@yandex.ru>
2022-04-05 21:21:17 +0300

    refactor: add MVC design

---
+++ CustomWeather/Controller/CityListController.swift
@@ -0,0 +1,16 @@
+//
+//  CityListController.swift
+//  CustomWeather
+//
+//  Created by Артем Свиридов on 05.04.2022.
+//
+
+import UIKit
+
+class CityListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
---
+++ CustomWeather/Services/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  CustomWeather
+//
+//  Created by Артем Свиридов on 04.04.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = CityListController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+}
+
